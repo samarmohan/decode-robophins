b408294e863b8bb000d6991ac1ebe763ac0d8797
Samar Mohan <samar.mohan@gmail.com>
2025-12-10 13:42:44 -0600

    finish old 12s, prolly won't be used

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/TwelveBlueCloseAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/TwelveBlueCloseAuton.java
@@ -15,25 +15,24 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
-import org.firstinspires.ftc.teamcode.auton.parts.Constants;
 import org.firstinspires.ftc.teamcode.auton.parts.Intake;
 import org.firstinspires.ftc.teamcode.auton.parts.Turret;
 
 
-@Autonomous(name = "RedCloseAuton")
-public class RedCloseAuton extends LinearOpMode {
+@Autonomous(name = "TwelveBlueCloseAuton")
+public class TwelveBlueCloseAuton extends LinearOpMode {
     @Override
     public void runOpMode() {
-        final double RED_SHOOT_ROTATION = Math.toRadians(135);
-        final double RED_COLLECT_ROTATION = Math.toRadians(90);
+        final double BLUE_SHOOT_ROTATION = Math.toRadians(-135);
+        final double BLUE_COLLECT_ROTATION = Math.toRadians(-90);
 
-        Pose2d initialPose = new Pose2d(-40, 53, RED_COLLECT_ROTATION);
-        Vector2d shooting = new Vector2d(-12, 12);
-        Vector2d collectFirstSet = new Vector2d(-12, 50);
-        Vector2d lineUpSecondSet = new Vector2d(12, 24);
-        Vector2d collectSecondSet = new Vector2d(12, 57);
-        Vector2d lineUpThirdSet = new Vector2d(36, 24);
-        Vector2d collectThirdSet = new Vector2d(36, 57);
+        Pose2d initialPose = new Pose2d(-40, -53, BLUE_COLLECT_ROTATION);
+        Vector2d shooting = new Vector2d(-12, -12);
+        Vector2d collectFirstSet = new Vector2d(-12, -50);
+        Vector2d lineUpSecondSet = new Vector2d(12, -24);
+        Vector2d collectSecondSet = new Vector2d(12, -57);
+        Vector2d lineUpThirdSet = new Vector2d(36, -24);
+        Vector2d collectThirdSet = new Vector2d(36, -57);
 
         MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
         Intake intake = new Intake(hardwareMap);
@@ -43,45 +42,45 @@ public class RedCloseAuton extends LinearOpMode {
         //Actions.runBlocking(new SequentialAction(claw.clawClose()));
 
         Action action = new ParallelAction(
-                intake.intakeIn(),
+                intake.intakeHold(),
                 turret.setFlywheelRPM(FLYWHEEL_RPM),
                 turret.setPitchPosition(PITCH_POSITION),
                 drive.actionBuilder(initialPose)
                         // shoot preset balls
-                        .strafeToSplineHeading(shooting, RED_SHOOT_ROTATION)
+                        .strafeToSplineHeading(shooting, BLUE_SHOOT_ROTATION)
                         .afterTime(0, intake.intakeShoot())
                         .waitSeconds(SHOOT_WAIT_TIME)
-                        .stopAndAdd(intake.intakeIn())
+                        .stopAndAdd(intake.intakeHold())
 
                         // collect first spike and shoot
-                        .turnTo(RED_COLLECT_ROTATION)
+                        .turnTo(BLUE_COLLECT_ROTATION)
                         .strafeTo(collectFirstSet)
                         .waitSeconds(COLLECT_WAIT_TIME)
-                        .strafeToSplineHeading(shooting, RED_SHOOT_ROTATION)
+                        .strafeToSplineHeading(shooting, BLUE_SHOOT_ROTATION)
                         .afterTime(0, intake.intakeShoot())
                         .waitSeconds(SHOOT_WAIT_TIME)
-                        .stopAndAdd(intake.intakeIn())
+                        .stopAndAdd(intake.intakeHold())
 
                         // collect second spike and shoot
-                        .strafeToSplineHeading(lineUpSecondSet, RED_COLLECT_ROTATION)
+                        .strafeToSplineHeading(lineUpSecondSet, BLUE_COLLECT_ROTATION)
                         .strafeTo(collectSecondSet)
                         .waitSeconds(COLLECT_WAIT_TIME)
-                        .strafeTo(new Vector2d(12, 50))
-                        .strafeToSplineHeading(shooting, RED_SHOOT_ROTATION)
+                        .strafeTo(new Vector2d(12, -50))
+                        .strafeToSplineHeading(shooting, BLUE_SHOOT_ROTATION)
                         .afterTime(0, intake.intakeShoot())
                         .waitSeconds(SHOOT_WAIT_TIME)
-                        .stopAndAdd(intake.intakeIn())
+                        .stopAndAdd(intake.intakeHold())
 
                         // collect third spike and shoot
-                        .strafeToSplineHeading(lineUpThirdSet, RED_COLLECT_ROTATION)
+                        .strafeToSplineHeading(lineUpThirdSet, BLUE_COLLECT_ROTATION)
                         .strafeTo(collectThirdSet)
                         .waitSeconds(COLLECT_WAIT_TIME)
-                        .strafeToSplineHeading(shooting, RED_SHOOT_ROTATION)
+                        .strafeToSplineHeading(shooting, BLUE_SHOOT_ROTATION)
                         .afterTime(0, intake.intakeShoot())
                         .waitSeconds(SHOOT_WAIT_TIME)
 
                         // reset
-                        .strafeToSplineHeading(collectFirstSet, RED_COLLECT_ROTATION)
+                        .strafeToSplineHeading(collectFirstSet, BLUE_COLLECT_ROTATION)
                         .afterTime(0, intake.intakeOff())
                         .afterTime(0, turret.setFlywheelRPM(0))
                         .afterTime(0, turret.setPitchPosition(0))
@@ -91,7 +90,8 @@ public class RedCloseAuton extends LinearOpMode {
 
         waitForStart();
         telemetry.update();
-            Actions.runBlocking(action);
 
+        if (isStopRequested()) return;
+        Actions.runBlocking(action);
     }
 }
